4e7355b5c4297a688f34895ae824730bfc0c9d6f
Anna <a.karlysheva@ukma.edu.ua>
2020-03-23 16:33:56 +0200

    base commit

---
+++ src/main/java/javaee/hw5/JavaEeHw5Application.java
@@ -0,0 +1,13 @@
+package javaee.hw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeHw5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEeHw5Application.class, args);
+    }
+
+}
---
+++ src/test/java/javaee/hw5/JavaEeHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package javaee.hw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeHw5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
